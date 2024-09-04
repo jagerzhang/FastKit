646678a8c6367b954103c75f1276df361f04e804
Jagerzhang <jagerzhang@tencent.com>
2024-09-04 14:14:27 +0800

    fix: 日志重复打印

--- setup.sh
+++ setup.sh
@@ -5,19 +5,12 @@ test -d build && rm -r build
 test -d dist && rm -r dist
 test -d *.egg-info && rm -r *.egg-info
 
-test -f .env && source .env
-
 python3 setup.py clean
 python3 setup.py sdist
 # python3 setup.py install
 
-if [ -z $repo_username -o -z $repo_password ];then
-    echo "repo_username or repo_password not found, plz add in .env file."
-    exit
-fi
-
 if [[ $publish -eq 1 ]];then
-    twine upload dist/*.tar.gz --repository-url https://mirrors.tencent.com/repository/pypi/tencent_pypi/simple --username=$repo_username --password=$repo_password
+    twine upload dist/*.tar.gz
 fi
 
 test -d build && rm -r build
