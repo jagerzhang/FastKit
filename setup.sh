173047b92f1c8a7be7618b27f4c7f3f476eeb3a1
Jagerzhang <jagerzhang@tencent.com>
2024-09-06 16:36:31 +0800

    feat: 优化后台调度

--- setup.sh
+++ setup.sh
@@ -7,12 +7,9 @@ test -d *.egg-info && rm -r *.egg-info
 
 python3 setup.py clean
 python3 setup.py sdist
-# python3 setup.py install
 
-if [[ $publish -eq 1 ]];then
-    twine upload dist/*.tar.gz
-fi
 
+twine upload dist/*.tar.gz
 test -d build && rm -r build
 test -d dist && rm -r dist
 test -d *.egg-info && rm -r *.egg-info
